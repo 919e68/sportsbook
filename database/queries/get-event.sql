7a0931f73106a828bc0db22e86844c1736ec0b58
wilson <konekred@gmail.com>
2017-11-09 18:02:03 +0800

    added get event query

---
+++ database/queries/get-event.sql
@@ -0,0 +1,38 @@
+DECLARE @eventId BIGINT = 1318157660876800
+
+SELECT 
+  Events.id AS [eventId],
+  Events.startTime,
+  Leagues.name AS [leagueName],
+  Sports.name AS [sportsName],   
+  Home.homeTeamName,
+  Away.awayTeamName,
+  Events.parentId
+FROM
+  Events LEFT JOIN
+  Sports ON Sports.id = Events.sportId LEFT JOIN
+  Events Leagues ON Leagues.id = Events.parentId LEFT JOIN
+    (
+    SELECT
+        EventParticipantRelations.eventId,
+        Participants.id AS homeTeamId,
+        Participants.name AS homeTeamName
+    FROM
+        EventParticipantRelations LEFT JOIN
+        Participants ON Participants.id = EventParticipantRelations.participantId
+    WHERE
+        EventParticipantRelations.participantRoleId = 1
+    ) AS Home ON Home.eventId = Events.id LEFT JOIN
+    (
+    SELECT
+        EventParticipantRelations.eventId,
+        Participants.id AS awayTeamId,
+        Participants.name AS awayTeamName
+    FROM
+        EventParticipantRelations LEFT JOIN
+        Participants ON Participants.id = EventParticipantRelations.participantId
+    WHERE
+        EventParticipantRelations.participantRoleId = 2
+    ) AS Away ON Away.eventId = Events.id
+WHERE
+   Events.id = @eventId
